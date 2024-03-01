1a74c4766ed5a1bb4efc8775117f869e2705ff37
Abed Rass <abd.assoum@outlook.com>
2024-03-01 15:03:00 +0100

    <Link> replaced instead of <a>

--- src/NavHeader.tsx
+++ src/NavHeader.tsx
@@ -6,20 +6,17 @@ export default function NavHeader() {
     <nav>
       <ul>
         <li>
-          <a href="/">Home</a>
-          {/* <Link to="/">Home</Link> */}
+          
+          <Link to="/">Home</Link>
         </li>
         <li>
-          <a href="/categories">Categories</a>
-          {/* <Link to="/categories">Categories</Link> */}
+          <Link to="/categories">Categories</Link>
         </li>
         <li>
-          <a href="/recipes">Recipes</a>
-          {/* <Link to="/recipes">Recipes</Link> */}
+          <Link to="/recipes">Recipes</Link>
         </li>
         <li>
-          <a href="/add">Add</a>
-          {/* <Link to="/add">Add</Link> */}
+          <Link to="/add">Add</Link>
         </li>
         <AuthStatus />
       </ul>
