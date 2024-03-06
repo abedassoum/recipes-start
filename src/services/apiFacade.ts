48c89c104c553ff0cb3a6b84d6ae67789539f67b
Abed Rass <abd.assoum@outlook.com>
2024-03-06 20:10:05 +0100

    added comment
    A comment that explains how to fetch Data from the backend and where to get the token from.

--- src/services/apiFacade.ts
+++ src/services/apiFacade.ts
@@ -37,9 +37,24 @@ async function getRecipes(category: string | null): Promise<Array<Recipe>> {
   const queryParams = category ? "?category=" + category : "";
   return fetch(RECIPE_URL + queryParams).then(handleHttpErrors);
 }
+
+// if you want the data to be fetched from the backend you need to get the token from the localstorage because its saved there.
+
+// async function getRecipes(category: string | null): Promise<Array<Recipe>> {
+//   const token = localStorage.getItem('token'); // Retrieve token from localStorage
+//   const headers = {
+//     'Authorization': `Bearer ${token}` // Include the token in the request headers
+//   };
+
+//   const queryParams = category ? "?category=" + category : "";
+//   const response = await fetch(RECIPE_URL + queryParams, { headers }); // Pass headers to fetch
+//   const recipes = await handleHttpErrors(response);
+//   return recipes;
+// }
+
 async function getRecipe(id: number): Promise<Recipe> {
   //if (recipes.length > 0) return [...recipes];
-  return fetch(RECIPE_URL + "/" + id).then(handleHttpErrors);  
+  return fetch(RECIPE_URL + "/" + id).then(handleHttpErrors);
 }
 async function addRecipe(newRecipe: Recipe): Promise<Recipe> {
   const method = newRecipe.id ? "PUT" : "POST";
